35425d61b9f2620e2f58c0b1970d795608454e3b
Restyled.io <commits@restyled.io>
2021-05-04 07:51:47 +0000

    Restyled by prettier

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -5,16 +5,21 @@ export const Input = (props) => {
 
   const checkAddressInput = (event) => {
     // TODO: how long can address be ?
-    const regExp = /\w{10,}/
+    const regExp = /\w{10,}/;
 
     if (!event.value.match(regExp)) {
-      event.preventDefault()
+      event.preventDefault();
     }
-  }
+  };
 
   if (type === 'address') {
     return (
-      <input className="input" type="text" placeholder="Recipient address" onChange={checkAddressInput} />
+      <input
+        className="input"
+        type="text"
+        placeholder="Recipient address"
+        onChange={checkAddressInput}
+      />
     );
   } else if (type === 'amount') {
     return <input className="input" type="number" placeholder="Amount" />;
